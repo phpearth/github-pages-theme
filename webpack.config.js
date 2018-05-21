ed4be9b401103996eaa72404d2a0a87f723ed09d
Peter Kokot <peterkokot@gmail.com>
2018-05-21 22:54:01 +0200

    Move dist to assets

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const config = {
     entry: './src/app.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'assets'),
         filename: 'bundle.js'
     },
     module: {
